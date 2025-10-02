297d24444222fe03791a2d579a44e991374564a4
Katherin <katherincoral@unicauca.edu.co>
2025-10-02 15:06:43 -0500

    Proyecto

---
+++ DesignPatterns/src/main/java/co/unicauca/designpatterns/DesignPatterns.java
@@ -0,0 +1,12 @@
+package co.unicauca.designpatterns;
+
+/**
+ *
+ * @author kthn1
+ */
+public class DesignPatterns {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
